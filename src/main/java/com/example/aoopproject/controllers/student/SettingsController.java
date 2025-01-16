8be987937cac9fb765300e1efb5e63a3d5adf7e5
Saif <639saifulislam@gmail.com>
2025-01-16 21:11:06 +0600

    add separate admin dashboard

--- src/main/java/com/example/aoopproject/controllers/student/SettingsController.java
+++
@@ -1,4 +0,0 @@
-package com.example.aoopproject.controllers.student;
-
-public class SettingsController {
-}
--- src/main/java/com/example/aoopproject/controllers/student/StudentController.java
+++ src/main/java/com/example/aoopproject/controllers/student/StudentController.java
@@ -1,35 +1,10 @@
 package com.example.aoopproject.controllers.student;
 
-import com.example.aoopproject.views.ViewFactory;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
+import javafx.scene.control.Tab;
 
 public class StudentController {
 
-    @FXML
-    public VBox adminVBox;
 
-    @FXML
-    public Button notificationsButton;
-
-    @FXML
-    public Button notesButton;
-
-    @FXML
-    public Button timerButton;
-
-    @FXML
-    public Button doubtsolvingButton;
-
-    @FXML
-    private Button logoutButton;
-
-    @FXML
-    private void handleLogoutButtonAction(ActionEvent ignoredEvent) {
-        Stage stage = (Stage) logoutButton.getScene().getWindow();
-        ViewFactory.getInstance().showLoginScreen(stage);
-    }
 }
